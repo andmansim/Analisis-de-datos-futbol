0bcc4582be3df9a2bb216d2f1f1029233fd5633f
andmansim <91722324+andmansim@users.noreply.github.com>
2024-02-10 13:48:31 +0100

    futbol :|

--- main.py
+++ main.py
@@ -5,5 +5,29 @@ Ejercicio de verdad
 2. Necesito la lista de los equpos que van a jugar en este año
 3. Puede que la lista de los partidos de los años anterirores
 4. Ns que más. 
+5. Hacer un master en futbol. 
+6. hacer gráficos de todos los años con las estadísticas de los últimos 10 años. 
+7. usar la sopa para extraer datos de webs (em cripto 9022024)
+'''
 
+'''
+¿Cómo funciona el fútbol?
+-Grupos: Los clubs de futbol se reparten en 8 grupos de 4 equipos cada uno. Cada equipo juega 6 partidos, 
+3 en casa y 3 fuera. En función del resutado se reparten una serie de puntos (3 por victoria, 
+1 por empate y 0 por derrota). Solo avanzan a la siguiente fase los dos primeros de cada grupo.
+
+-Eliminatorias: Estas empiezan después de los grupos. Se compone de los octavos, cuartos, semifinales y final.
+  -Octavos: Después de los grupos, los 16 equipos que han pasado se emparejan en 8 enfrentamientos.
+  Enfrentamientos: son dos partidos, 1 en casa y otro fuera. Pasa a la siguiente ronda el equipo que haya 
+  marcado más goles en total. Si hay un empate en goles, se decidirá bajo unas ciertas reglas. 
+  Se da más importancia a los goles marcados fuera de casa y se suele seguir esto para dictar quien pasa.  
+  
+  
+  -Cuartos: Los 8 equipos ganadores se emparejan en 4 enfrentamientos. Sigue el mismo reghlamento que los 
+  octavos. 
+  
+  -Semifinal: los mismo pero con 4 equipos.
+  
+  -Final: Los dos equipos que han ganado en las semifinales se enfrentan en un partido.
+  
 '''
