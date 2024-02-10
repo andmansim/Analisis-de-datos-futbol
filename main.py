5438a17f58f0a9971a42241ea16723d8933a3803
andmansim <91722324+andmansim@users.noreply.github.com>
2024-02-10 13:18:17 +0100

    orden en el caos

---
+++ main.py
@@ -0,0 +1,9 @@
+'''
+Ejercicio de verdad
+1. Encontrar las estadísticas de uefa de los años anteriores, para poder procesar los datos.
+   Estas deben de tener PP, PE, PG, PJ, Puntos, Equipo. 
+2. Necesito la lista de los equpos que van a jugar en este año
+3. Puede que la lista de los partidos de los años anterirores
+4. Ns que más. 
+
+'''
