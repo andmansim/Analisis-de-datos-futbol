a7aab44dc90f2799ccd09d0194900b21b5511ef5
andmansim <91722324+andmansim@users.noreply.github.com>
2024-02-16 11:56:20 +0100

    iniccio

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ import matplotlib.pyplot as plt
 
 
 #leemos el csv
-df = pd.read_csv('datos_fut.csv')
-print(df.head())    
+df = pd.read_csv('datos_fut.csv', encoding='utf-8', sep=';')
+print(df.head())  
+
+#vemos las columnas y los datos
+print(df.columns)
+print(df.info())  
 
     
