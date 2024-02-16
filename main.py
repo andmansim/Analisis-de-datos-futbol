1ce486592c1994fc206e06b29b3319c52a19f63b
andmansim <91722324+andmansim@users.noreply.github.com>
2024-02-16 12:40:07 +0100

    ahora si

--- main.py
+++ main.py
@@ -11,6 +11,16 @@ print(df.head())
 print(df.columns)
 print(df.info())  
 
+#Remplazamos las comas por puntos
+df = df.replace(',', '.', regex=True)
+print(df.head())
+#convertimos las columnas a float
+for i in df.columns:
+    if 'por' in i:
+        df[i] = df[i].astype(float)
+print(df.info())
+
+
 #Representación de los datos
 def grafi_barras(dato1, dato2, titulo, x, y):
     plt.figure(figsize=(12,5))
@@ -23,12 +33,13 @@ def grafi_barras(dato1, dato2, titulo, x, y):
 
 def grafi_baras_colores(dato1, titulo, x, y):
     plt.figure(figsize=(12,5))
-    plt.bar(dato1, color=['green', 'blue', 'red'], stacked=True)
+    dato1.plot(kind='bar', color=['green', 'blue', 'red'], stacked=True)
     plt.title(titulo)
     plt.xlabel(x)
     plt.xticks(rotation=45)
     plt.ylabel(y)
     plt.show()
+    
 #Gráficos de barras, 10 que más han ganado la champions
 df_ordenado = df.sort_values(by='Titles', ascending=False)
 df_ordenado = df_ordenado.head(10)
@@ -40,6 +51,6 @@ equipo_pais = df['Country'].value_counts()
 grafi_barras(equipo_pais.index, equipo_pais.values, 'Equipos por país', 'País', 'Número de equipos')
 
 #Gráfico de barras mostrando los el % de ataque, defensa de 10 mejores equipos
-df_por = df.set_index('Club')[["porganarpartido", 'porempatarpartido', 'porperderpartido']]
-grafi_baras_colores(df_por.head(10), 'Porcentaje de ataque, defensa y empate de los 10 mejores equipos', 'Equipo', 'Porcentaje')
-
+df_por = df.set_index('Club')[["porganarpartido", 'poremppartido', 'porperderpartido']]
+#grafi_baras_colores(df_por.head(10), 'Porcentaje de ataque, defensa y empate de los 10 mejores equipos', 'Equipo', 'Porcentaje')
+print(df_por.head(10))
