e90c888090d3173ad6eec00882c68ea67aecf384
andmansim <91722324+andmansim@users.noreply.github.com>
2024-02-16 12:15:13 +0100

    graficas

--- main.py
+++ main.py
@@ -11,4 +11,23 @@ print(df.head())
 print(df.columns)
 print(df.info())  
 
-    
+#Representación de los datos
+def grafi_barras(dato1, dato2, titulo, x, y):
+    plt.figure(figsize=(10,5))
+    plt.bar(dato1, dato2)
+    plt.title(titulo)
+    plt.xlabel(x)
+    plt.ylabel(y)
+    plt.show()
+
+df_ordenado = df.sort_values(by='Titles', ascending=False)
+df_ordenado = df_ordenado.head(10)
+grafi_barras(df_ordenado['Club'], df_ordenado['Titles'], 'Número de veces que ha ganado la champions cada equipo', 'Equipo', 'Número de veces')
+
+
+#Gráfico de barras, mostramos cuantos equipos hay por país
+equipo_pais = df['Country'].value_counts()
+grafi_barras(equipo_pais.index, equipo_pais.values, 'Equipos por país', 'País', 'Número de equipos')
+
+
+ 
