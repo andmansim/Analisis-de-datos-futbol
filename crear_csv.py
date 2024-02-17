4a31365bf50e14644394338cb9f022f10185271c
andmansim <91722324+andmansim@users.noreply.github.com>
2024-02-17 22:28:42 +0100

    añadimos datos al csv

--- crear_csv.py
+++ crear_csv.py
@@ -28,13 +28,12 @@ df_partidos = pd.DataFrame(columns=columnas)
 
 #añadimos los datos de los grupos por días
 df_partidos['fecha'] =['19/09/23', '19/09/23','19/09/23', '19/09/23', '19/09/23', '19/09/23','19/09/23', '19/09/23']
-df_partidos['local'] =['AC Milan', 'young boys', 'psg', 'shakhtar', 'manchester city', 'lazio', 'barcelona', 'feyenoord']
-df_partidos['visitante'] =['Newcastle United FC', 'rb leipzig', 'dortmund', 'porto', 'estrella roja', 'altético madrid', 'antwerp', 'celtic fc']
+df_partidos['local'] =['AC Milan', 'BSC Young Boys', 'Paris Saint-Germain', 'FC Shakhtar Donetsk', 'Manchester City FC', 'SS Lazio', 'FC Barcelona', 'Feyenoord']
+df_partidos['visitante'] =['Newcastle United FC', 'RB Leipzig', 'Borussia Dortmund', 'FC Porto', 'FK Crvena zvezda', 'Club Atlético de Madrid', 'Royal Antwerp FC', 'Celtic FC']
 print(df_partidos.head()) 
 
 def prob_enfrentada(equipo1, equipo2):
-    
-    
+
     #Recogemos los datos de cada equipo
     prob_gana_local = df_equipos[df_equipos['Club']== equipo1]['porganarpartido'].values[0]
     prob_gana_visitante = df_equipos[df_equipos['Club'] == equipo2]['porganarpartido'].values[0]
@@ -49,6 +48,12 @@ def prob_enfrentada(equipo1, equipo2):
     prob_empate1 = round((prob_empate_local * prob_empate_visitante)/100, 2)
 
     return prob_gana_local1, prob_empate1, prob_gana_visitante1
-prob_ganar_local, prob_empate, prob_gana_visitante = prob_enfrentada(df_partidos['local'][0], df_partidos['visitante'][0])
-print(prob_ganar_local, prob_empate, prob_gana_visitante)
+
+#Añadimos las probabilidades enfrentadas al dataframe
+for i, fila in df_partidos.iterrows():
+    prob_ganar_local, prob_empate, prob_gana_visitante = prob_enfrentada(fila['local'], fila['visitante'])
+    df_partidos.at[i, 'prob_ganar_local'] = prob_ganar_local
+    df_partidos.at[i, 'porb_empate'] = prob_empate
+    df_partidos.at[i, 'prob_ganar_visitante'] = prob_gana_visitante
+print(df_partidos.head())
 
