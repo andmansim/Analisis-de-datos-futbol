a522d60a4954792826781254c48a1919517ce6c2
andmansim <91722324+andmansim@users.noreply.github.com>
2024-02-18 00:08:25 +0100

    de momento ya

--- crear_csv.py
+++ crear_csv.py
@@ -1,23 +1,4 @@
 
-'''
-¿Cómo funciona el fútbol?
--Grupos: Los clubs de futbol se reparten en 8 grupos de 4 equipos cada uno. Cada equipo juega 6 partidos, 
-3 en casa y 3 fuera. En función del resutado se reparten una serie de puntos (3 por victoria, 
-1 por empate y 0 por derrota). Solo avanzan a la siguiente fase los dos primeros de cada grupo.
--Eliminatorias: Estas empiezan después de los grupos. Se compone de los octavos, cuartos, semifinales y final.
-  -Octavos: Después de los grupos, los 16 equipos que han pasado se emparejan en 8 enfrentamientos.
-  Enfrentamientos: son dos partidos, 1 en casa y otro fuera. Pasa a la siguiente ronda el equipo que haya 
-  marcado más goles en total. Si hay un empate en goles, se decidirá bajo unas ciertas reglas. 
-  Se da más importancia a los goles marcados fuera de casa y se suele seguir esto para dictar quien pasa.  
-  
-  
-  -Cuartos: Los 8 equipos ganadores se emparejan en 4 enfrentamientos. Sigue el mismo reghlamento que los 
-  octavos. 
-  
-  -Semifinal: los mismo pero con 4 equipos.
-  
-  -Final: Los dos equipos que han ganado en las semifinales se enfrentan en un partido.
-'''
 import pandas as pd
 #leemos el csv con los datos de cadda equipo
 df_equipos = pd.read_csv('datos_fut.csv', encoding='utf-8', sep=';')
@@ -27,9 +8,9 @@ columnas = ['fecha', 'local', 'visitante', 'prob_ganar_local', 'porb_empate', 'p
 df_partidos = pd.DataFrame(columns=columnas)
 
 #añadimos los datos de los grupos por días
-df_partidos['fecha'] =['19/09/23', '19/09/23','19/09/23', '19/09/23', '19/09/23', '19/09/23','19/09/23', '19/09/23']
-df_partidos['local'] =['AC Milan', 'BSC Young Boys', 'Paris Saint-Germain', 'FC Shakhtar Donetsk', 'Manchester City FC', 'SS Lazio', 'FC Barcelona', 'Feyenoord']
-df_partidos['visitante'] =['Newcastle United FC', 'RB Leipzig', 'Borussia Dortmund', 'FC Porto', 'FK Crvena zvezda', 'Club Atlético de Madrid', 'Royal Antwerp FC', 'Celtic FC']
+df_partidos['fecha'] = ['13/02/24', '13/02/24', '14/02/24', '14/02/24', '20/02/24', '20/02/24', '21/02/24', '21/02/24', '5/03/24', '5/03/24', '6/03/24', '6/03/24', '12/03/24', '12/03/24', '13/03/24', '13/03/24']
+df_partidos['local'] =['B 1903 København','RB Leipzig', 'SS Lazio','Paris Saint-Germain', 'FC Inter Turku' , 'PSV Eindhoven', 'FC Porto', 'SSC Napoli', 'FC Bayern München', 'Real Sociedad de Fútbol', 'Manchester City FC', 'Real Madrid CF', 'Arsenal FC', 'FC Barcelona', 'Club Atlético de Madrid', 'Borussia Dortmund']
+df_partidos['visitante'] =['Manchester City FC', 'Real Madrid CF', 'FC Bayern München', 'Real Sociedad de Fútbol', 'Club Atlético de Madrid', 'Borussia Dortmund', 'Arsenal FC', 'FC Barcelona', 'SS Lazio', 'Paris Saint-Germain', 'B 1903 København', 'RB Leipzig', 'FC Porto', 'SSC Napoli', 'FC Inter Turku', 'PSV Eindhoven'] 
 print(df_partidos.head()) 
 
 def prob_enfrentada(equipo1, equipo2):
