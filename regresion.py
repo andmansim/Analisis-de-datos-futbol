62b490426e5509bfb5e7ff60f94582bf2d5dfd39
andmansim <91722324+andmansim@users.noreply.github.com>
2024-02-18 17:45:28 +0100

    jjj

--- regresion.py
+++ regresion.py
@@ -21,6 +21,7 @@ def regresion ( df_partidos):
     #Dividimos los datos en entrenamiento y prueba
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+    print(df_partidos.describe())
     #Creamos el modelo
     modelo = LogisticRegression()
     #Entrenamos el modelo
