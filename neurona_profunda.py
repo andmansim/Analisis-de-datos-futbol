70bc03e5bd55ede5e17eec8a5955ae8cfdd6c879
andmansim <91722324+andmansim@users.noreply.github.com>
2024-03-08 11:34:11 +0100

    mmm

--- neurona_profunda.py
+++ neurona_profunda.py
@@ -3,3 +3,4 @@ Durante el entrenamiento se le proporciona un conjunto de datos de entrada y sal
 y el algoritmo ajusta los pesos de las conexiones entre las neuronas para minimizar el error 
 entre la salida esperada y la salida real.
 '''
+import torch 
