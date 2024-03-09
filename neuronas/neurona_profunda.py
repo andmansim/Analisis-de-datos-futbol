5d80397b60d0c3ad8e2130409df3886ff2985140
Andrea Manuel Simón <91722324+andmansim@users.noreply.github.com>
2024-03-09 18:20:19 +0100

    :(

--- neuronas/neurona_profunda.py
+++ neuronas/neurona_profunda.py
@@ -5,8 +5,20 @@ entre la salida esperada y la salida real.
 '''
 import torch
 import torch.nn as nn
-import torch.optim as optim
-import numpy as np
+import torch.utils.data as td
+
+
+#importamos librerias
+torch.manual_seed(0)
+print('Se han importado las librerías, listo para usar\n', torch.__version__)
+
+#preparamos los datos 
+#creamos un dataset con los datos de train
+train_x = torch.Tensor(x_train).float()
+train_y = torch.Tensor(y_train).long()
+train_ds = td.TensorDataset(train_x, train_y)
+train_loader = td.DataLoader(train_ds, batch_size=20, shuffle=False, num_workers=1)
+
 
 
 
