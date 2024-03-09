5c8a741781228bc16125a9e52db19f422b42bfaf
Andrea Manuel Simón <91722324+andmansim@users.noreply.github.com>
2024-03-09 14:47:42 +0100

    funca

--- neuronas/neurona_profunda.py
+++ neuronas/neurona_profunda.py
@@ -3,7 +3,11 @@ Durante el entrenamiento se le proporciona un conjunto de datos de entrada y sal
 y el algoritmo ajusta los pesos de las conexiones entre las neuronas para minimizar el error 
 entre la salida esperada y la salida real.
 '''
-import torch 
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+
 
 
 
