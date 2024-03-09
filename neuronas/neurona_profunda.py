10d90f9c4c532b67813ef751ce407f49b39805c2
Andrea Manuel Simón <91722324+andmansim@users.noreply.github.com>
2024-03-09 19:02:47 +0100

    clasificams

--- neuronas/neurona_profunda.py
+++ neuronas/neurona_profunda.py
@@ -6,6 +6,28 @@ entre la salida esperada y la salida real.
 import torch
 import torch.nn as nn
 import torch.utils.data as td
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from matplotlib import pyplot as plt
+
+
+#Aquí vamos a clasificar los equipos en tres categorías, ataque, defensa o neutro
+#dependiendo de ello les asociaremos un número 0, 1 o 2
+
+#leemos el archivo
+df_equipos = pd.read_csv('csvs/datos_fut.csv', delimiter=';', encoding='utf-8')
+
+#clasificamos los equipos
+def clasificar_equipos(row):
+    if row['porgfav/gtot'] > 60:
+        return 0
+    elif row['porgfav/gtot'] < 40:
+        return 1
+    else:
+        return 2
+
 
 
 #importamos librerias
