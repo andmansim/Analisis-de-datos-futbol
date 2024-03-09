16ef1af897682a3c845ef5132fe0b894d1dafec6
Andrea Manuel Simón <91722324+andmansim@users.noreply.github.com>
2024-03-09 19:09:04 +0100

    separemos en train y test

--- neuronas/neurona_profunda.py
+++ neuronas/neurona_profunda.py
@@ -21,19 +21,36 @@ df_equipos = pd.read_csv('csvs/datos_fut.csv', delimiter=';', encoding='utf-8')
 
 #clasificamos los equipos
 def clasificar_equipos(row):
-    if row['porgfav/gtot'] > 60:
-        return 0
-    elif row['porgfav/gtot'] < 40:
-        return 1
+    if row['porgfav/gtot'] > 50:
+        return 0 #ataque
+    elif row['porgfav/gtot'] < 50:
+        return 1 #defensa
     else:
-        return 2
+        return 2 #neutro
 
+#creamos una nueva columna en el dataframe
+df_equipos['categoria'] = df_equipos.apply(clasificar_equipos, axis=1)
 
+#guardamos los datos en un nuevo archivo
+df_equipos.to_csv('csvs/datos_fut_clasificados.csv', index=False)
 
-#importamos librerias
+#Quitamos las columnas que no numéricas
+df_equipos = df_equipos.drop(['Club', 'Country'], axis=1)
+
+#creamos semilla
 torch.manual_seed(0)
 print('Se han importado las librerías, listo para usar\n', torch.__version__)
 
+#Separamos los datos en train y test
+x = df_equipos.drop(['porganarpartido', 'porperderpartido', 'poremppartido'], axis=1)
+y = df_equipos['categoria']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+print('Datos separados en train y test\n')
+print('x_train:', x_train.shape
+      , 'x_test:', x_test.shape
+      , 'y_train:', y_train.shape
+      , 'y_test:', y_test.shape)
 #preparamos los datos 
 #creamos un dataset con los datos de train
 train_x = torch.Tensor(x_train).float()
