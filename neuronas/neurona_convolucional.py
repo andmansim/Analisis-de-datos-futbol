aa740f9d0ba59ebb7c331fb2f22cdf6abe7dfc1c
Andrea Manuel Simón <91722324+andmansim@users.noreply.github.com>
2024-03-26 11:42:14 +0100

    librerias

---
+++ neuronas/neurona_convolucional.py
@@ -0,0 +1,13 @@
+
+#Importamos las librerías
+import torch
+import torchvison
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision.transforms as transforms
+import torch.autograd as Variable
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
