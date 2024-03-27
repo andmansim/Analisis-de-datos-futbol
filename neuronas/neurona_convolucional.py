9a1ee85d449d29647715ba8ff590d91a251fae76
Andrea Manuel Simón <91722324+andmansim@users.noreply.github.com>
2024-03-27 13:21:38 +0100

    cambios

--- neuronas/neurona_convolucional.py
+++ neuronas/neurona_convolucional.py
@@ -13,17 +13,14 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import torch.utils.data as td
-import torchvision.transforms as transforms
-import torch.autograd as Variable
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 import pandas as pd
 
 #Cargamos los datos
-df_equipos = pd.read_csv('csvs/datos_fut_lasificados.csv', encoding='utf-8', delimiter=',')
+df_equipos = pd.read_csv('csvs/datos_fut_clasificados.csv', encoding='utf-8', delimiter=',')
 
 #Eliminamos las variables categóricas
 df_equipos = df_equipos.drop(['Club', 'Country'], axis=1)
@@ -51,32 +48,34 @@ print('Datos cargados\n')
 #creamos la red neuronal
 class Net(nn.Module):
     #constructor
-    def __init__(self, num_classes=3):
+    def __init__(self, input_dim, num_classes):
         super(Net, self).__init__()
         #definimos las capas de la red
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=12, kernel_size=3, stride=1, padding=1)
-        self.pool = nn.MaxPool2d(kernel_size=2)
-        
-        self.conv2 = nn.Conv2d(in_channels=12, out_channels=12, kernel_size=3, stride=1, padding=1)
-        self.conv3 = nn.Conv2d(in_channels=12, out_channels=24, kernel_size=3, stride=1, padding=1)
-        
-        self.drop = nn.Dropout2d(p=0.2)
-        
-        self.fc = nn.Linear(in_features=32*32*24, out_features=num_classes)
+        self.conv1 = nn.Conv2d(in_channels=input_dim, out_channels=16, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
+    
+        self.fc1 = nn.Linear(64*input_dim, 128)
+        self.fc2 = nn.Linear(128, num_classes)
     
     def forward(self, x):
         x = F.relu(self.conv1(x))
-        
+        x = F.max_pool1d(x, 2)
         x = F.relu(self.conv2(x))
-        
+        x= F.max_pool1d(x, 2)
         x = F.relu(self.conv3(x))
-
-        x = F.dropout(x, training=self.training)
-        
-        x = x.view(-1, 32*32*24)
-
-        x = self.fc(x)
+        x = F.max_pool1d(x, 2)
+        x = x.view(-1, self.num_flat_features(x))   
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
         return F.log_softmax(x, dim=1)
+
+    def num_flat_features(self, x):
+        size = x.size()[1:]
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
     
 print('Red Neuronal creada\n')
 
@@ -134,58 +133,60 @@ if torch.cuda.is_available():
 print('Usando:', device)
 
 #creamos el modelo
-model = Net(num_classes=3).to(device)
+input_dim = len(x_train.columns)
+model = Net(input_dim, 3).to(device)
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 
 loss_criteria = nn.CrossEntropyLoss()
+if __name__ == '__main__':
+    epoch_nums = []
+    training_loss = []
+    validation_loss = []
 
-epoch_nums = []
-training_loss = []
-validation_loss = []
+    #entrenamos el modelo
+    epochs = 5
+    for epoch in range(1, epochs + 1):
+        train_loss = train(model, device, train_loader, optimizer, epoch)
+        test_loss = test(model, device, test_loader)
+        epoch_nums.append(epoch)
+        training_loss.append(train_loss)
+        validation_loss.append(test_loss)
+        
+    #mostramos la gráfica de la pérdida
+    plt.plot(epoch_nums, training_loss)
+    plt.plot(epoch_nums, validation_loss)
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.legend(['training', 'validation'], loc='upper right')
+    plt.show()
 
-#entrenamos el modelo
-epochs = 5
-for epoch in range(1, epochs + 1):
-    train_loss = train(model, device, train_loader, optimizer, epoch)
-    test_loss = test(model, device, test_loader)
-    epoch_nums.append(epoch)
-    training_loss.append(train_loss)
-    validation_loss.append(test_loss)
-    
-#mostramos la gráfica de la pérdida
-plt.plot(epoch_nums, training_loss)
-plt.plot(epoch_nums, validation_loss)
-plt.xlabel('epoch')
-plt.ylabel('loss')
-plt.legend(['training', 'validation'], loc='upper right')
-plt.show()
+    #evaluamos el modelo
+    model.eval()
+    print('Mostrando predicciones\n')
+    truelabels = []
+    predictions = []
+    for data, target in test_loader:
+        data, target = data.to(device), target.to(device)
+        for label in target.cpu().data.numpy():
+            truelabels.append(label)
+        for prediction in model(data).cpu().data.numpy().argmax(1):
+            predictions.append(prediction)
 
-#evaluamos el modelo
-model.eval()
-print('Mostrando predicciones\n')
-truelabels = []
-predictions = []
-for data, target in test_loader:
-    for label in target.cpu().data.numpy():
-        truelabels.append(label)
-    for prediction in model.cpu()(data).data.numpy().argmax(1):
-        predictions.append(prediction)
+    cm = confusion_matrix(truelabels, predictions)
+    plt.imshow(cm, interpolation="nearest", cmap=plt.cm.Blues)
+    plt.colorbar()
+    clases = ['defensa', 'ataque', 'neutro']
+    tick_marks = np.arange(len(clases))
+    plt.xticks(tick_marks, clases, rotation=45)
+    plt.yticks(tick_marks, clases)
+    plt.xlabel('Predicho')
+    plt.ylabel('Real')
+    plt.show()
 
-cm = confusion_matrix(truelabels, predictions)
-plt.imshow(cm, interpolation="nearest", cmap=plt.cm.Blues)
-plt.colorbar()
-clases = ['defensa', 'ataque', 'neutro']
-tick_marks = np.arange(len(clases))
-plt.xticks(tick_marks, clases, rotation=45)
-plt.yticks(tick_marks, clases)
-plt.xlabel('Predicho')
-plt.ylabel('Real')
-plt.show()
+    #guardamos el modelo
+    model_file = 'formas_model.pth'
+    torch.save(model.state_dict(), model_file)
+    del model
+    print('Modelo guardado en', model_file) 
 
-#guardamos el modelo
-model_file = 'formas_model.pth'
-torch.save(model.state_dict(), model_file)
-del model
-print('Modelo guardado en', model_file) 
-
-#Cargamos el modelo
+    #Cargamos el modelo
