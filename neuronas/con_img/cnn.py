9b32aee0b4dd58bed3ed127f3df716002077f3a3
Andrea Manuel Simón <91722324+andmansim@users.noreply.github.com>
2024-04-05 13:14:12 +0200

    mmmmmm

--- neuronas/con_img/cnn.py
+++ neuronas/con_img/cnn.py
@@ -1,4 +1,4 @@
-# Import PyTorch libraries
+
 import torch
 import torchvision
 import torchvision.transforms as transforms
@@ -6,16 +6,14 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.autograd import Variable
 import torch.nn.functional as F
-
-# Other libraries we'll use
 import numpy as np
 import os
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
 
-# The images are in the data/shapes folder
-data_path = 
+#cogemos la carpeta img
+data_path = os.path.join(os.path.dirname(__file__), 'img')
 
 # Get the class names
 classes = os.listdir(data_path)
@@ -23,11 +21,12 @@ classes.sort()
 print(len(classes), 'classes:')
 print(classes)
 
-# Show the first image in each folder
+#Mostremos una imagen de cada clase
 fig = plt.figure(figsize=(8, 12))
 i = 0
 for sub_dir in os.listdir(data_path):
     i+=1
+    
     img_file = os.listdir(os.path.join(data_path,sub_dir))[0]
     img_path = os.path.join(data_path, sub_dir, img_file)
     img = mpimg.imread(img_path)
@@ -37,29 +36,25 @@ for sub_dir in os.listdir(data_path):
     a.set_title(img_file)
 plt.show()
 
-# Function to ingest data using training and test loaders
+# Función para cargar el conjunto de datos
 def load_dataset(data_path):
-    # Load all of the images
+    # Cargar todas las imágenes, transformándolas
+    # a tensores y normalizándolas con una media y desviación estándar específicas
+    #transformation = transforms.Compose([transforms.ToTensor(),transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])])
     transformation = transforms.Compose([
-        # transform to tensors
-        transforms.ToTensor(),
-        # Normalize the pixel values (in R, G, and B channels)
-        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+        transforms.Resize((225, 225)),  # Redimensiona todas las imágenes a 225x225
+        transforms.ToTensor(),  # Convierte las imágenes a tensores
+        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])  # Normaliza las imágenes
     ])
-
-    # Load all of the images, transforming them
-    full_dataset = torchvision.datasets.ImageFolder(
-        root=data_path,
-        transform=transformation
-    )
+    # Cargar todas las imágenes, transformamos
+    full_dataset = torchvision.datasets.ImageFolder(root=data_path,transform=transformation)
     
-    
-    # Split into training (70% and testing (30%) datasets)
+    #Dividimos en conjuntos de entrenamiento (70%) y prueba (30%)
     train_size = int(0.7 * len(full_dataset))
     test_size = len(full_dataset) - train_size
     train_dataset, test_dataset = torch.utils.data.random_split(full_dataset, [train_size, test_size])
     
-    # define a loader for the training data we can iterate through in 50-image batches
+    # Preparamos los dataloaders para entrenamiento y prueba
     train_loader = torch.utils.data.DataLoader(
         train_dataset,
         batch_size=50,
@@ -67,7 +62,7 @@ def load_dataset(data_path):
         shuffle=False
     )
     
-    # define a loader for the testing data we can iterate through in 50-image batches
+    # Preparamos los dataloaders para prueba
     test_loader = torch.utils.data.DataLoader(
         test_dataset,
         batch_size=50,
@@ -77,100 +72,77 @@ def load_dataset(data_path):
         
     return train_loader, test_loader
 
-
-# Get the iterative dataloaders for test and training data
+#Separamos los datos en train y test
 train_loader, test_loader = load_dataset(data_path)
-print('Data loaders ready')
+print('Datos listos para usar\n')
 
-# Create a neural net class
+#Creamos la red neuronal 
 class Net(nn.Module):
     # Constructor
     def __init__(self, num_classes=3):
         super(Net, self).__init__()
-        
-        # Our images are RGB, so input channels = 3. We'll apply 12 filters in the first convolutional layer
+        #creamos las capas de la red
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=12, kernel_size=3, stride=1, padding=1)
         
-        # We'll apply max pooling with a kernel size of 2
         self.pool = nn.MaxPool2d(kernel_size=2)
         
-        # A second convolutional layer takes 12 input channels, and generates 12 outputs
         self.conv2 = nn.Conv2d(in_channels=12, out_channels=12, kernel_size=3, stride=1, padding=1)
         
-        # A third convolutional layer takes 12 inputs and generates 24 outputs
         self.conv3 = nn.Conv2d(in_channels=12, out_channels=24, kernel_size=3, stride=1, padding=1)
         
-        # A drop layer deletes 20% of the features to help prevent overfitting
         self.drop = nn.Dropout2d(p=0.2)
-        
-        # Our 128x128 image tensors will be pooled twice with a kernel size of 2. 128/2/2 is 32.
-        # So our feature tensors are now 32 x 32, and we've generated 24 of them
-        # We need to flatten these and feed them to a fully-connected layer
-        # to map them to  the probability for each class
-        self.fc = nn.Linear(in_features=32 * 32 * 24, out_features=num_classes)
+
+        self.fc = nn.Linear(in_features=75264, out_features=num_classes)
 
     def forward(self, x):
-        # Use a relu activation function after layer 1 (convolution 1 and pool)
         x = F.relu(self.pool(self.conv1(x)))
-      
-        # Use a relu activation function after layer 2 (convolution 2 and pool)
         x = F.relu(self.pool(self.conv2(x)))
-        
-        # Select some features to drop after the 3rd convolution to prevent overfitting
         x = F.relu(self.drop(self.conv3(x)))
-        
-        # Only drop the features if this is a training pass
         x = F.dropout(x, training=self.training)
-        
-        # Flatten
-        x = x.view(-1, 32 * 32 * 24)
-        # Feed to fully-connected layer to predict class
+        #Aplanamos los datos        
+        x = x.view(x.size(0), -1)
+      
         x = self.fc(x)
-        # Return log_softmax tensor 
+        
         return F.log_softmax(x, dim=1)
     
-print("CNN model class defined!")
+print('Clase cnn lista\n')
 
 def train(model, device, train_loader, optimizer, epoch):
-    # Set the model to training mode
+    #Entrenamos el modelo
     model.train()
     train_loss = 0
     print("Epoch:", epoch)
-    # Process the images in batches
+    # Procesamos las imágenes en lotes
     for batch_idx, (data, target) in enumerate(train_loader):
-        # Use the CPU or GPU as appropriate
+        # Enviamos los datos al dispositivo
         data, target = data.to(device), target.to(device)
-        
-        # Reset the optimizer
+        # Reseteamos el optimizador
         optimizer.zero_grad()
-        
-        # Push the data forward through the model layers
+        # Pasamos los datos a través del modelo
         output = model(data)
-        
-        # Get the loss
+        # Obtenemos la pérdida
         loss = loss_criteria(output, target)
-        
-        # Keep a running total
+        # Mantenemos un total en ejecución
         train_loss += loss.item()
-        
-        # Backpropagate
+        # Realizamos la retropropagación
         loss.backward()
         optimizer.step()
         
-        # Print metrics for every 10 batches so we see some progress
+        # Mostramos las métricas para cada 10 lotes para ver el progreso
         if batch_idx % 10 == 0:
             print('Training set [{}/{} ({:.0f}%)] Loss: {:.6f}'.format(
                 batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.item()))
-            
-    # return average loss for the epoch
+
+    # Devolvemos la pérdida promedio para la época
     avg_loss = train_loss / (batch_idx+1)
     print('Training set: Average loss: {:.6f}'.format(avg_loss))
     return avg_loss
             
             
 def test(model, device, test_loader):
-    # Switch the model to evaluation mode (so we don't backpropagate or drop)
+    # Evaluamos el modelo
     model.eval()
     test_loss = 0
     correct = 0
@@ -179,51 +151,45 @@ def test(model, device, test_loader):
         for data, target in test_loader:
             batch_count += 1
             data, target = data.to(device), target.to(device)
-            
-            # Get the predicted classes for this batch
+            #Con el modelo, predecimos las clases
             output = model(data)
             
-            # Calculate the loss for this batch
+            #Calculamos la perdida de cada lote          
             test_loss += loss_criteria(output, target).item()
-            
-            # Calculate the accuracy for this batch
+            #Obtenemos el número de predicciones correctas
             _, predicted = torch.max(output.data, 1)
             correct += torch.sum(target==predicted).item()
 
-    # Calculate the average loss and total accuracy for this epoch
+    #Calculamos la perdida promedio y la precisión total para la época
     avg_loss = test_loss/batch_count
     print('Validation set: Average loss: {:.6f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         avg_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
     
-    # return average loss for the epoch
     return avg_loss
     
     
-# Now use the train and test functions to train and test the model    
 
 device = "cpu"
 if (torch.cuda.is_available()):
-    # if GPU available, use cuda (on a cpu, training will take a considerable length of time!)
+    # Si hay GPU disponible, usamos cuda
     device = "cuda"
 print('Training on', device)
-
-# Create an instance of the model class and allocate it to the device
+# Creamos una instancia de la clase del modelo y la asignamos al dispositivo
 model = Net(num_classes=len(classes)).to(device)
 
-# Use an "Adam" optimizer to adjust weights
-# (see https://pytorch.org/docs/stable/optim.html#algorithms for details of supported algorithms)
+#Usamos el optimizador Adam para ajustar los pesos
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-# Specify the loss criteria
+#Especificamos el criterio de pérdida
 loss_criteria = nn.CrossEntropyLoss()
 
-# Track metrics in these arrays
+#Listas para almacenar las métricas
 epoch_nums = []
 training_loss = []
 validation_loss = []
 
-# Train over 5 epochs (in a real scenario, you'd likely use many more)
+#Entrenamos durante 5 épocas (en un escenario real, probablemente usarías muchas más)
 epochs = 5
 for epoch in range(1, epochs + 1):
         train_loss = train(model, device, train_loader, optimizer, epoch)
