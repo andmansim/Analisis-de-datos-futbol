7ebc79e99b0b173006f3cda2ab7a4352b0c23e12
Andrea Manuel Simón <91722324+andmansim@users.noreply.github.com>
2024-04-15 11:47:20 +0200

    ns

--- datos.py
+++ datos.py
@@ -9,9 +9,9 @@ soup = BeautifulSoup(html, 'html.parser')
 # Utiliza el selector CSS para encontrar la tabla
 tabla_jugadores = soup.select('body > div.main-wrap > div > div > div.content > div.d3-react.statistics-detail.pk-theme--light > div > div > div:nth-child(4) > div > div.HuCvdCPuILEJ2xfnIMRi > div > div.ag-root-wrapper.ag-ltr.ag-layout-auto-height > div.ag-root-wrapper-body.ag-focus-managed.ag-layout-auto-height > div.ag-root.ag-unselectable.ag-layout-auto-height > div.ag-body.ag-layout-auto-height > div.ag-body-viewport.ag-row-animation.ag-layout-auto-height')
 
-for fila in  tabla_jugadores.find_all('tr'):
+for fila in tabla_jugadores:
     datos_jugador ={}
-    for celda in fila.find_all('td'):
+    for celda in fila.find('td'):
         if 'nombre' not in datos_jugador:
             datos_jugador['nombre'] = celda.text.strip()
         elif 'equipo' not in datos_jugador:
