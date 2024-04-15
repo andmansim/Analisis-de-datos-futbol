8110b01d0a7543baa8cc6b0ec5a72c5d4c5de903
Andrea Manuel Simón <91722324+andmansim@users.noreply.github.com>
2024-04-15 11:43:16 +0200

    veamos

--- datos.py
+++ datos.py
@@ -5,4 +5,21 @@ url = 'https://www.uefa.com/uefachampionsleague/statistics/players/?sortBy=minut
 response = requests.get(url)
 html = response.text
 soup = BeautifulSoup(html, 'html.parser')
-players = soup.find_all('td', class_='player')
+
+# Utiliza el selector CSS para encontrar la tabla
+tabla_jugadores = soup.select('body > div.main-wrap > div > div > div.content > div.d3-react.statistics-detail.pk-theme--light > div > div > div:nth-child(4) > div > div.HuCvdCPuILEJ2xfnIMRi > div > div.ag-root-wrapper.ag-ltr.ag-layout-auto-height > div.ag-root-wrapper-body.ag-focus-managed.ag-layout-auto-height > div.ag-root.ag-unselectable.ag-layout-auto-height > div.ag-body.ag-layout-auto-height > div.ag-body-viewport.ag-row-animation.ag-layout-auto-height')
+
+for fila in  tabla_jugadores.find_all('tr'):
+    datos_jugador ={}
+    for celda in fila.find_all('td'):
+        if 'nombre' not in datos_jugador:
+            datos_jugador['nombre'] = celda.text.strip()
+        elif 'equipo' not in datos_jugador:
+            datos_jugador['equipo'] = celda.text.strip()
+        elif 'minutos' not in datos_jugador:
+            datos_jugador['minutos'] = celda.text.strip()
+        elif 'goles' not in datos_jugador:
+            datos_jugador['goles'] = celda.text.strip()
+        elif 'asistencias' not in datos_jugador:
+            datos_jugador['asistencias'] = celda.text.strip()
+    print(datos_jugador)
