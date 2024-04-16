a05d9e4560e58d262856f15266e7746b82db8d8a
Andrea Manuel Simón <91722324+andmansim@users.noreply.github.com>
2024-04-16 11:01:45 +0200

    ns

--- datos.py
+++ datos.py
@@ -1,25 +1,33 @@
 from bs4 import BeautifulSoup
 import requests
+from selenium import webdriver
 
+
+driver = webdriver.Chrome()
 url = 'https://www.uefa.com/uefachampionsleague/statistics/players/?sortBy=minutes_played_official&order=aschttps://www.uefa.com/uefachampionsleague/statistics/players/?sortBy=minutes_played_official&order=asc'
-response = requests.get(url)
-html = response.text
+driver.get(url)
+
+driver.implicitly_wait(10)
+
+html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
 
 # Utiliza el selector CSS para encontrar la tabla
 #tabla_jugadores = soup.select('body > div.main-wrap > div > div > div.content > div.d3-react.statistics-detail.pk-theme--light > div > div > div:nth-child(4) > div > div.HuCvdCPuILEJ2xfnIMRi > div > div.ag-root-wrapper.ag-ltr.ag-layout-auto-height > div.ag-root-wrapper-body.ag-focus-managed.ag-layout-auto-height > div.ag-root.ag-unselectable.ag-layout-auto-height > div.ag-body.ag-layout-auto-height > div.ag-body-viewport.ag-row-animation.ag-layout-auto-height')
-#NO COGE LOS DATOS, DA UNA LONGITUD DE 0
-tabla_jugadores  = soup.find_all(attrs={'role':'presentation'})
+
+tabla_jugadores  = soup.select('div', {'role':"presentation"})
 datos = soup.find_all(attrs={'role':'gridcell'})
 for fila in tabla_jugadores:
-    print(fila)
-    nombre = fila.find_all('span', slot='primary')
-    equipo = fila.find_all('span', slot='secondary')
+    print(fila) 
+    nombre = fila.select('div', {'class': 'primary'})
+    #nombre = fila.select('span', {'slot': 'primary'})
+    equipo = fila.select('span', slot='secondary')
     print(nombre)
     print(equipo)
     minutos = fila.find_all('span', class_='ag-cell--sorted')
     partidos = fila.find_all('span', class_='ag-cell-value')
-    
+
+driver.quit()
 '''for celda in datos:
     print(celda)
     minutos = celda.find_all()    
