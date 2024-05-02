949ac7dcd78d8454e0479f28a962ecba400a66de
Andrea Manuel Simón <91722324+andmansim@users.noreply.github.com>
2024-05-02 12:02:48 +0200

    comparamos

---
+++ regresion/crear_csv1.py
@@ -0,0 +1,59 @@
+
+import pandas as pd
+#leemos el csv con los datos de cadda equipo
+df_equipos = pd.read_csv('csvs/datos_fut.csv', encoding='utf-8', sep=';')
+
+#creamos el dataframe 
+columnas = ['fecha', 'local', 'visitante','puntos_local', 'puntos_visitante', 'resultado']
+df_partidos = pd.DataFrame(columns=columnas)
+
+#añadimos los datos de los grupos por días
+df_partidos['fecha'] = ['13/02/24', '13/02/24', '14/02/24', '14/02/24', '20/02/24', '20/02/24', '21/02/24', '21/02/24', '5/03/24', '5/03/24', '6/03/24', '6/03/24', '12/03/24', '12/03/24', '13/03/24', '13/03/24']
+df_partidos['local'] =['B 1903 København','RB Leipzig', 'SS Lazio','Paris Saint-Germain', 'FC Inter Turku' , 'PSV Eindhoven', 'FC Porto', 'SSC Napoli', 'FC Bayern München', 'Real Sociedad de Fútbol', 'Manchester City FC', 'Real Madrid CF', 'Arsenal FC', 'FC Barcelona', 'Club Atlético de Madrid', 'Borussia Dortmund']
+df_partidos['visitante'] =['Manchester City FC', 'Real Madrid CF', 'FC Bayern München', 'Real Sociedad de Fútbol', 'Club Atlético de Madrid', 'Borussia Dortmund', 'Arsenal FC', 'FC Barcelona', 'SS Lazio', 'Paris Saint-Germain', 'B 1903 København', 'RB Leipzig', 'FC Porto', 'SSC Napoli', 'FC Inter Turku', 'PSV Eindhoven'] 
+
+
+def datos_nuevos(equipo1, equipo2):
+    #Recogemos los datos de cada equipo
+    porcapacidad_ofensiva_local = df_equipos[df_equipos['club']== equipo1]['porcapacidad_ofensiva'].values[0]
+    porcapacidad_ofensiva_visitante = df_equipos[df_equipos['club'] == equipo2]['porcapacidad_ofensiva'].values[0]
+    porcapacidad_defensiva_local = df_equipos[df_equipos['club'] == equipo1]['porcapacidad_defensiva'].values[0]
+    porcapacidad_defensiva_visitante = df_equipos[df_equipos['club'] == equipo2]['porcapacidad_defensiva'].values[0]
+    
+    #comparamos los datosy les asignamos un número
+    puntos_local = 0
+    puntos_visitante = 0
+    
+    #Ofensiva A vs Ofensiva B 
+    if porcapacidad_ofensiva_local > porcapacidad_ofensiva_visitante:
+        puntos_local += 1
+    else: 
+        puntos_visitante += 1
+    
+    #Ofensiva A vs Defensiva B
+    if porcapacidad_ofensiva_local > porcapacidad_defensiva_visitante:
+        puntos_local += 1
+    else: 
+        puntos_visitante += 1
+    
+    #Defensiva A vs Ofensiva B
+    if porcapacidad_defensiva_local > porcapacidad_ofensiva_visitante:
+        puntos_local += 1
+    else: 
+        puntos_visitante += 1
+        
+   
+    return puntos_local, puntos_visitante
+
+def actualizar_datos(df_partidos):
+    #añadimos los datos recogido anteriormente a un dataframe
+    for i, fila in df_partidos.iterrows():
+        puntos_local, puntos_visitante = datos_nuevos(fila['local'], fila['visitante'])
+        df_partidos.at[i, 'puntos_local'] = puntos_local
+        df_partidos.at[i, 'puntos_visitante'] = puntos_visitante
+    return df_partidos
+
+df_partidos = actualizar_datos(df_partidos)
+#lo pasamos a csv
+df_partidos.to_csv('csvs1/partidos_fut.csv', sep=';', index=False, encoding='utf-8')
+
---
+++ regresion/regresion1.py
@@ -0,0 +1,104 @@
+'''
+Regresion logistica se utiliza cuando la variable de respuesta es binaria. Debe de ser un número entero.
+Se utiliza para clasificar, no para predecir valores continuos. Ejemplo: predecir si un correo es spam o no.
+
+Regresion lineal es para ver la relación entre dos variables.
+Se utiliza para predecir valores continuos, ejemplo: predecir el precio de una casa.
+'''
+#Regresión logística
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix
+import pandas as pd
+from crear_csv import actualizar_probabilidades
+
+def regresion ( df_partidos):
+    
+    #Comparamos las probabilidades, para poder entrenar el modelo en función de estas
+    #No pueden ser str tiene que ser int para que se puedan comparar y entrenarlo
+    df_partidos['resultado'] = int(1) #local
+    df_partidos.loc[df_partidos['puntos_local'] < df_partidos['puntos_visitante'], 'resultado'] = int(2) #visitante
+    df_partidos.loc[df_partidos['puntos_local'] == df_partidos['puntos_visitante'], 'resultado'] = int(3) #empate
+    
+    print(df_partidos['resultado'])
+    
+    #separamos las variables independientes y dependientes
+    X = df_partidos[['puntos_local', 'puntos_visitante']]
+    y = df_partidos['resultado']
+    
+    #Dividimos los datos en entrenamiento y prueba
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, )
+    print(X_train, X_test, y_train, y_test)
+    #Creamos el modelo
+    modelo = LogisticRegression()
+    #Entrenamos el modelo
+    modelo.fit(X_train, y_train)
+
+    #Hacemos las predicciones
+    y_pred = modelo.predict(X_test)
+
+    #Calculamos la precisión
+    print('Precisión del modelo:', accuracy_score(y_test, y_pred))
+
+    #Matriz de confusión
+    print(confusion_matrix(y_test, y_pred))
+
+    #Mostramos los resultados de cada partido que hemos obtenido de las predicciones
+    predicciones = modelo.predict(X)
+    for i, fila in df_partidos.iterrows():
+        if predicciones[i] == 1:
+            resultado = 'Local'
+        elif predicciones[i] == 2:
+            resultado = 'Visitante'
+        else:
+            resultado = 'Empate'
+            
+        print('\n' + fila['local'], 'vs', fila['visitante'], 'Resultado:', resultado)
+    
+    return df_partidos
+
+#leemos el csv
+df_partido = pd.read_csv('csvs1/partidos_fut.csv', encoding='utf-8', sep=';')
+
+print('Ronda 1')
+#llamamos a la función regresión
+df_partido = regresion(df_partido)
+#actualizamos los datos del csv con los resultados obtenidos
+df_partido.to_csv('csvs1/partidos_fut.csv', index=False, sep=';')
+
+'''
+#en función de los resultados creamos un nuevo dataframe con los siguientes partidos
+df_partidos_ronda2 = pd.DataFrame(columns=['local', 'visitante', 'prob_ganar_local', 'prob_empate', 'prob_ganar_visitante', 'resultado'])
+df_partidos_ronda2['local']= ['Paris Saint-Germain', 'FC Barcelona', 'Club Atlético de Madrid', 'FC Bayern München', 'Arsenal FC', 'Manchester City FC', 'Borussia Dortmund', 'Real Madrid CF']
+df_partidos_ronda2['visitante'] = ['Arsenal FC', 'Manchester City FC', 'Borussia Dortmund', 'Real Madrid CF', 'Paris Saint-Germain', 'FC Barcelona', 'Club Atlético de Madrid', 'FC Bayern München']
+df_partidos_ronda2 = actualizar_probabilidades(df_partidos_ronda2)
+
+print('Ronda 2')
+df_partidos_ronda2 = regresion(df_partidos_ronda2)
+df_partidos_ronda2.to_csv('csvs/partidos_fut_ronda2.csv', index=False, sep=';')
+
+#Ronda 3
+df_partidos_ronda3 = pd.DataFrame(columns=['local', 'visitante', 'prob_ganar_local', 'prob_empate', 'prob_ganar_visitante', 'resultado'])
+df_partidos_ronda3['local']= ['Paris Saint-Germain', 'Club Atlético de Madrid', 'FC Barcelona', 'FC Bayern München']
+df_partidos_ronda3['visitante'] = ['FC Barcelona', 'FC Bayern München','Paris Saint-Germain',  'Club Atlético de Madrid' ]
+df_partidos_ronda3 = actualizar_probabilidades(df_partidos_ronda3)
+
+print('Ronda 3')
+df_partidos_ronda3 = regresion(df_partidos_ronda3)
+df_partidos_ronda3.to_csv('csvs/partidos_fut_ronda3.csv', index=False, sep=';')
+
+#Ronda 4
+df_partidos_ronda4 = pd.DataFrame(columns=['local', 'visitante', 'prob_ganar_local', 'prob_empate', 'prob_ganar_visitante', 'resultado'])
+df_partidos_ronda4['local']= ['FC Barcelona', 'FC Bayern München']
+df_partidos_ronda4['visitante'] = ['FC Bayern München', 'FC Barcelona']
+df_partidos_ronda4 = actualizar_probabilidades(df_partidos_ronda4)
+
+print('Ronda 4')
+#cogemos los dos % de ganar de cada equipo y los comparamos
+if df_partidos_ronda4['prob_ganar_local'][0] < df_partidos_ronda4['prob_ganar_visitante'][1]:
+    print('Gana el visitante', df_partidos_ronda4['visitante'][1])
+else:
+    print('Gana el local', df_partidos_ronda4['local'][0])
+
+
+'''
